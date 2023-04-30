04084d9b666fe5a6d4ab7f26c6d18528a86a8bc3
Damian Majsner <d.majsner@gmail.com>
2023-04-30 20:47:33 +0200

    update docs to functional component

--- docs/index.tsx
+++ docs/index.tsx
@@ -3,105 +3,28 @@ import * as ReactDOM from 'react-dom'
 import StatusAlert, { StatusAlertService } from '../dist'
 import { AlertOptions } from '../dist/status-alert-item'
 import '../dist/status-alert.css'
+import { useState } from 'react'
 
-export interface DemoAppState {
-  inputText: string
+const buttonStyle = {
+  color: '#fff',
+  padding: '5px 10px',
+  margin: '0px 5px',
+  backgroundColor: '#3231FF',
 }
 
-export class DemoApp extends React.Component<{}, DemoAppState> {
-  public constructor(props: any) {
-    super(props)
+const separatorStyle = {
+  margin: '10px 0px',
+}
 
-    this.state = {
-      inputText: '',
-    }
-  }
+const flashingAlertOptions: AlertOptions = {
+  withCloseIcon: false,
+  autoHide: true,
+}
 
-  public render() {
-    return (
-      <div>
-        <StatusAlert />
-        <div className="hero">
-          <div className="hero__content">
-            <div className="row">
-              <div className="col-xs center-xs">
-                <h1 className="hero__title">ReactJS Status Alert</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="wrapper">
-          <h1>ReactJS Status Alert</h1>
-          <p className="badges">
-            <a href="https://npmjs.org/package/react-status-alert">
-              <img src="https://img.shields.io/npm/v/react-status-alert.svg" className="badge" />
-            </a>
-            <a href="https://github.com/daymosik/react-status-alert/actions?query=workflow%3Abuild">
-              <img src="https://github.com/daymosik/react-status-alert/workflows/build/badge.svg" className="badge" />
-            </a>
-            <a href="https://codebeat.co/projects/github-com-daymosik-react-status-alert-master">
-              <img src="https://codebeat.co/badges/2ca97e65-d7fa-4f72-aeea-e0f8bd17765c" className="badge" />
-            </a>
-            <a href="https://bettercodehub.com/">
-              <img
-                src="https://bettercodehub.com/edge/badge/daymosik/react-status-alert?branch=master"
-                className="badge"
-              />
-            </a>
-            <a href="https://codecov.io/gh/daymosik/react-status-alert">
-              <img
-                src="https://codecov.io/gh/daymosik/react-status-alert/branch/master/graph/badge.svg"
-                className="badge"
-              />
-            </a>
-          </p>
-          <p>Simple React Status Alert component with Typescript support.</p>
-          <h2>Installation</h2>
-          <p>The package can be installed via NPM:</p>
-          <p>
-            <code>npm install react-status-alert --save</code>
-          </p>
-        </div>
+function DemoApp() {
+  const [inputText, handleInputText] = useState<string>('')
 
-        <div className="wrapper">
-          <h1>Examples</h1>
-          <button onClick={this.showSuccess} style={{ ...this.buttonStyle, backgroundColor: '#107c2e' }}>
-            Show success alert
-          </button>
-          <button onClick={this.showError} style={{ ...this.buttonStyle, backgroundColor: '#ff3b20' }}>
-            Show error alert
-          </button>
-          <button onClick={this.showInfo} style={{ ...this.buttonStyle, backgroundColor: '#464c55' }}>
-            Show info alert
-          </button>
-          <button onClick={this.showWarning} style={{ ...this.buttonStyle, backgroundColor: '#f90' }}>
-            Show warning alert
-          </button>
-
-          <hr style={this.separatorStyle} />
-
-          <input type="text" value={this.state.inputText} onChange={this.handleInput} />
-          <button onClick={this.showTextAlert} style={{ ...this.buttonStyle }}>
-            Show alert
-          </button>
-
-          <hr style={this.separatorStyle} />
-
-          <button onClick={this.showHtmlAlert} style={{ ...this.buttonStyle }}>
-            Show HTML alert
-          </button>
-
-          <hr style={this.separatorStyle} />
-
-          <button onClick={this.showSuccessWithNoRemove} style={{ ...this.buttonStyle }}>
-            Show success alert without removing others
-          </button>
-        </div>
-      </div>
-    )
-  }
-
-  public showHtmlAlert = (): void => {
+  const showHtmlAlert = (): void => {
     StatusAlertService.showInfo(
       <div>
         HTML alert with <a href="">link</a>
@@ -109,56 +32,116 @@ export class DemoApp extends React.Component<{}, DemoAppState> {
     )
   }
 
-  public handleInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    this.setState({ inputText: event.target.value })
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    handleInputText(event.target.value)
   }
 
-  public showTextAlert = () => StatusAlertService.showInfo(this.state.inputText || 'Custom alert text')
+  const showTextAlert = () => StatusAlertService.showInfo(inputText || 'Custom alert text')
 
-  public showSuccess = (): void => {
-    StatusAlertService.showSuccess('Success!', this.flashingAlertOptions)
+  const showSuccess = (): void => {
+    StatusAlertService.showSuccess('Success!', flashingAlertOptions)
   }
 
-  public showError = (): void => {
-    StatusAlertService.showError('Oops... Something went wrong.', this.flashingAlertOptions)
+  const showError = (): void => {
+    StatusAlertService.showError('Oops... Something went wrong.', flashingAlertOptions)
   }
 
-  public showInfo = (): void => {
-    StatusAlertService.showInfo('Notification!', this.flashingAlertOptions)
+  const showInfo = (): void => {
+    StatusAlertService.showInfo('Notification!', flashingAlertOptions)
   }
 
-  public showWarning = (): void => {
-    StatusAlertService.showWarning('Warning!', this.flashingAlertOptions)
+  const showWarning = (): void => {
+    StatusAlertService.showWarning('Warning!', flashingAlertOptions)
   }
 
-  public showSuccessWithNoRemove = (): void => {
+  const showSuccessWithNoRemove = (): void => {
     StatusAlertService.showSuccess('Success with remove all!', {
-      ...this.flashingAlertOptions,
+      ...flashingAlertOptions,
       removeAllBeforeShow: false,
     })
   }
 
-  get buttonStyle() {
-    return {
-      color: '#fff',
-      padding: '5px 10px',
-      margin: '0px 5px',
-      backgroundColor: '#3231FF',
-    }
-  }
+  return (
+    <div>
+      <StatusAlert />
+      <div className="hero">
+        <div className="hero__content">
+          <div className="row">
+            <div className="col-xs center-xs">
+              <h1 className="hero__title">ReactJS Status Alert</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="wrapper">
+        <h1>ReactJS Status Alert</h1>
+        <p className="badges">
+          <a href="https://npmjs.org/package/react-status-alert">
+            <img src="https://img.shields.io/npm/v/react-status-alert.svg" className="badge" />
+          </a>
+          <a href="https://github.com/daymosik/react-status-alert/actions?query=workflow%3Abuild">
+            <img src="https://github.com/daymosik/react-status-alert/workflows/build/badge.svg" className="badge" />
+          </a>
+          <a href="https://codebeat.co/projects/github-com-daymosik-react-status-alert-master">
+            <img src="https://codebeat.co/badges/2ca97e65-d7fa-4f72-aeea-e0f8bd17765c" className="badge" />
+          </a>
+          <a href="https://bettercodehub.com/">
+            <img
+              src="https://bettercodehub.com/edge/badge/daymosik/react-status-alert?branch=master"
+              className="badge"
+            />
+          </a>
+          <a href="https://codecov.io/gh/daymosik/react-status-alert">
+            <img
+              src="https://codecov.io/gh/daymosik/react-status-alert/branch/master/graph/badge.svg"
+              className="badge"
+            />
+          </a>
+        </p>
+        <p>Simple React Status Alert component with Typescript support.</p>
+        <h2>Installation</h2>
+        <p>The package can be installed via NPM:</p>
+        <p>
+          <code>npm install react-status-alert --save</code>
+        </p>
+      </div>
 
-  get separatorStyle() {
-    return {
-      margin: '10px 0px',
-    }
-  }
+      <div className="wrapper">
+        <h1>Examples</h1>
+        <button onClick={showSuccess} style={{ ...buttonStyle, backgroundColor: '#107c2e' }}>
+          Show success alert
+        </button>
+        <button onClick={showError} style={{ ...buttonStyle, backgroundColor: '#ff3b20' }}>
+          Show error alert
+        </button>
+        <button onClick={showInfo} style={{ ...buttonStyle, backgroundColor: '#464c55' }}>
+          Show info alert
+        </button>
+        <button onClick={showWarning} style={{ ...buttonStyle, backgroundColor: '#f90' }}>
+          Show warning alert
+        </button>
 
-  get flashingAlertOptions(): AlertOptions {
-    return {
-      withCloseIcon: false,
-      autoHide: true,
-    }
-  }
+        <hr style={separatorStyle} />
+
+        <input type="text" value={inputText} onChange={handleInput} />
+        <button onClick={showTextAlert} style={{ ...buttonStyle }}>
+          Show alert
+        </button>
+
+        <hr style={separatorStyle} />
+
+        <button onClick={showHtmlAlert} style={{ ...buttonStyle }}>
+          Show HTML alert
+        </button>
+
+        <hr style={separatorStyle} />
+
+        <button onClick={showSuccessWithNoRemove} style={{ ...buttonStyle }}>
+          Show success alert without removing others
+        </button>
+      </div>
+    </div>
+  )
 }
 
 ReactDOM.render(<DemoApp />, document.getElementById('app'))
